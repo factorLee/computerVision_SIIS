e060f83ab6c9c676ba4c1b81d592ac45a8ff180d
sangjuLee <53315807+factorLee@users.noreply.github.com>
2021-07-09 09:17:35 +0900

    Delete cv_tensoflow_study3 directory

--- cv_tensoflow_study3/google_search4.py
+++
@@ -1,8 +0,0 @@
-from googlesearch import search
-import ssl
-
-ssl._create_default_https_context = ssl._create_unverified_context
-query = input("무엇을 검색하고 싶으신가요?")
-
-for i in search(query, start=0, stop=10):
-    print(i)
